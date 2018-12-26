ad5d8eaa7abad094298a12a3f7db4351f2764a72
AStrizh <abstrizhevskiy@gmail.com>
2018-12-26 12:57:07 -0500

    Implemented map based services

---
+++ petclinicdata/src/main/java/services/map/OwnerServiceMap.java
@@ -0,0 +1,35 @@
+package services.map;
+
+import com.sun.xml.internal.bind.v2.model.core.ID;
+import model.Owner;
+import services.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteByID(id);
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+}
